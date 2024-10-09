b66a86bdc042575c1a04ec450df7a6fced3ae417
Bakhodir <106083101+Bakhodir95@users.noreply.github.com>
2024-10-09 20:11:02 +0500

    saved

--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:svg_flutter/svg.dart';
 
 class RegisterScreen extends StatelessWidget {
   const RegisterScreen({super.key});
@@ -13,18 +13,7 @@ class RegisterScreen extends StatelessWidget {
             padding: const EdgeInsets.all(20),
             child: Column(
               children: [
-                Container(
-                  width: 160.w,
-                  height: 67.h,
-                  decoration: BoxDecoration(
-                    image: DecorationImage(
-                      image: const AssetImage("assets/images/header.png"),
-                      fit: BoxFit.contain, // Ensures the image scales properly
-                    ),
-                  ),
-                ),
-                // Add other widgets here as needed
-              ],
+                Image.asset("assets/images/header.png")],
             ),
           ),
         ),
