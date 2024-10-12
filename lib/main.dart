b5fbf08045572b7d7a0dca41f973f7c2271a93d9
Bakhodir <106083101+Bakhodir95@users.noreply.github.com>
2024-10-12 21:10:05 +0500

    screenutils

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const ScreenUtilInit(
-      
+      designSize: Size(412, 899),
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         home: LocationScreen(),
